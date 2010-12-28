15cef2d622fb776b00f2d509b5bf9cd5df4f176a
jboydston <jboydston@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-12-28 21:51:47 +0000

    moved class to separate file
    git-svn-id: https://plugins.svn.wordpress.org/export-posts/trunk@326806 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ export-posts-class.php
@@ -0,0 +1,41 @@
+<?php
+class Export_Posts_Walker_Category_Checklist extends Walker {
+	var $tree_type = 'category';
+	var $db_fields = array ('parent' => 'parent', 'id' => 'term_id'); //TODO: decouple this
+
+	function start_lvl(&$output, $depth, $args) {
+		$indent = str_repeat("\t", $depth);
+		$output .= "$indent<ul class='children'>\n";
+	}
+
+	function end_lvl(&$output, $depth, $args) {
+		$indent = str_repeat("\t", $depth);
+		$output .= "$indent</ul>\n";
+	}
+
+	function start_el(&$output, $category, $depth, $args) {
+		extract($args);
+		if ( empty($taxonomy) )
+			$taxonomy = 'category';
+
+		if ( $taxonomy == 'category' )
+			$name = 'post_category';
+		else
+			$name = 'tax_input['.$taxonomy.']';
+
+        $selected = array();
+        if ($_GET['post_category']) {
+		    if  (in_array($category->term_id, $_GET['post_category'])) { 
+		        $selected = array($category->term_id);
+	        }
+	    }
+
+		$class = in_array( $category->term_id, $popular_cats ) ? ' class="popular-category"' : '';
+		$output .= "\n<li id='{$taxonomy}-{$category->term_id}'$class>" . '<label class="selectit"><input value="' . $category->term_id . '" type="checkbox" name="'.$name.'[]" id="in-'.$taxonomy.'-' . $category->term_id . '"' . checked( in_array( $category->term_id, $selected ), true, false ) . disabled( empty( $args['disabled'] ), false, false ) . ' /> ' . esc_html( apply_filters('the_category', $category->name )) . '</label>';
+	}
+
+	function end_el(&$output, $category, $depth, $args) {
+		$output .= "</li>\n";
+	}
+}
+?>
